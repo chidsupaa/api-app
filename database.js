961b68c8570d85f383ac755dea29cc5d42f7e983
chidsupa campapan <chidsupa.ca@ksu.ac.th>
2024-10-10 18:36:20 +0700

    first commit

---
+++ database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'employ',
+    password: '',
+});
+
+module.exports = pool.promise();
